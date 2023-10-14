d4b95e7a9020b30c900e51291cb37504752226e9
Poncho <ameneses67@gmail.com>
2023-10-14 00:13:51 -0600

    Build platform icon list

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, CardHeader, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../services/game-service";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
 	game: Game;
@@ -13,7 +14,15 @@ const GameCard = ({ game }: Props) => {
 		>
 			<Image src={game.background_image} />
 			<CardBody>
-				<CardHeader>{game.name}</CardHeader>
+				<Heading
+					fontSize={"2xl"}
+					marginBottom={2}
+				>
+					{game.name}
+				</Heading>
+				<PlatformIconList
+					platforms={game.parent_platforms.map((platform) => platform.platform)}
+				/>
 			</CardBody>
 		</Card>
 	);
