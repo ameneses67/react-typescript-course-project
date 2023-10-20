655ab345afbde5835a98fb27d38b9b57589c6be6
Poncho <ameneses67@gmail.com>
2023-10-19 18:13:38 -0600

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,13 @@
-import { List, ListItem, HStack, Image, Text, Spinner } from "@chakra-ui/react";
+import {
+	List,
+	ListItem,
+	HStack,
+	Image,
+	Text,
+	Spinner,
+	Button,
+	Heading,
+} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 import useGenres, { Genre } from "../hooks/useGenres";
 
@@ -19,38 +28,49 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 	});
 
 	return (
-		<List
-			spacing="4"
-			position="relative"
-		>
-			{isLoading && (
-				<Spinner
-					position="absolute"
-					marginLeft="50%"
-					marginTop="50%"
-				/>
-			)}
-			{error && null}
-			{dataSortedByName.map((genre) => (
-				<ListItem key={genre.id}>
-					<HStack>
-						<Image
-							boxSize="32px"
-							borderRadius={8}
-							src={getCroppedImageUrl(genre.image_background)}
-						/>
-						<Text
-							onClick={() => onSelectGenre(genre)}
-							fontSize="lg"
-							cursor="pointer"
-							fontWeight={genre.id === selectedGenre?.id ? "black" : "normal"}
-						>
-							{genre.name}
-						</Text>
-					</HStack>
-				</ListItem>
-			))}
-		</List>
+		<>
+			<Heading
+				my={5}
+				fontSize="2xl"
+			>
+				Genres
+			</Heading>
+			<List
+				spacing="4"
+				position="relative"
+			>
+				{isLoading && (
+					<Spinner
+						position="absolute"
+						marginLeft="50%"
+						marginTop="50%"
+					/>
+				)}
+				{error && null}
+				{dataSortedByName.map((genre) => (
+					<ListItem key={genre.id}>
+						<HStack>
+							<Image
+								boxSize="32px"
+								borderRadius={8}
+								objectFit="cover"
+								src={getCroppedImageUrl(genre.image_background)}
+							/>
+							<Button
+								onClick={() => onSelectGenre(genre)}
+								variant="link"
+								fontSize="lg"
+								whiteSpace="normal"
+								textAlign="left"
+								fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+							>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
+				))}
+			</List>
+		</>
 	);
 };
 
